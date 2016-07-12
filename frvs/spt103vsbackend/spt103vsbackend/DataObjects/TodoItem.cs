ccc4be3ac7843338841865e70ee74dbd173db7b6
Fabian Williams <fabian@adotob.com>
2016-07-11 23:33:54 -0400

    started off creating the VS Mobile App .NET backend

---
+++ frvs/spt103vsbackend/spt103vsbackend/DataObjects/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Mobile.Server;
+
+namespace spt103vsbackend.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+        public double SessionGrade { get; set; }
+        public string AttendeeFullName { get; set; }
+        public string AttendeeEmail { get; set; }
+        public bool Complete { get; set; }
+    }
+}
---
+++ frvs/spt103vsbackend/spt103vsbackend/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(spt103vsbackend.Startup))]
+
+namespace spt103vsbackend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
